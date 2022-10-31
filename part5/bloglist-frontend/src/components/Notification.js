f2c0423e1500245e7ee9d9a4eb682b77d52f6356
VincTaff <vincent122416@gmail.com>
2022-10-31 18:36:20 +0100

    add propType for notification component

--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Notification = ({ notification }) => {
   if (!notification) {
@@ -18,6 +19,10 @@ const Notification = ({ notification }) => {
     </div>
   )
 
+}
+
+Notification.propTypes = {
+  notification: PropTypes.string
 }
 
 export default Notification
